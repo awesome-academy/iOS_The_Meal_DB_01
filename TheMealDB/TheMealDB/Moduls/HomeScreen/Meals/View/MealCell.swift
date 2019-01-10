6c9db37ab353f2cf7f85744f4d99092d10d1bcc8
sunset12321215 <sunsetno08@gmail.com>
2019-01-10 20:37:39 +0700

    Design Home Screen

--- TheMealDB/TheMealDB/Moduls/HomeScreen/Meals/View/MealCell.swift
+++ TheMealDB/TheMealDB/Moduls/HomeScreen/Meals/View/MealCell.swift
@@ -8,30 +8,9 @@
 
 import UIKit
 
-class MealCell: UITableViewCell {
-    //  MARK: - UI Propertis
-    private let mealView = CustomMealView()
-    
-    //  MARK: - Life Cycle
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        addSubview(mealView)
-        mealView.snp.makeConstraints { (make) in
-            make.top.left.equalToSuperview()
-            make.width.equalToSuperview()
-            make.height.equalTo(200)
-        }
-    }
-    
-    //  MARK: - Configuage
-    func configuage(data: Meal) {
-        guard let url = URL(string: data.strMealThumb) else {
-            return
-        }
-        mealView.mealThumbImage.sd_setImage(with: url, completed: nil)
-        mealView.categoryText = data.strCategory
-        mealView.mealText = data.strMeal
-        mealView.idMealText = data.idMeal
-        mealView.areaText = data.strArea
-    }
+class MealCell: UICollectionViewCell {
+    //  MARK: - UI Element
+    @IBOutlet weak private var mealImage: UIImageView!
+    @IBOutlet weak private var categoryNameLabel: UILabel!
+    @IBOutlet weak private var mealNameLabel: UILabel!
 }
